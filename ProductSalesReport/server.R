092b32f49fff88b4deb414a7f3b91a640065b5c7
dhruvang <dhruvang.jhaveri@gmail.com>
2016-05-25 12:56:05 +0530

    Uploading App files and data

--- ProductSalesReport/server.R
+++ ProductSalesReport/server.R
@@ -1,26 +1,36 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(dplyr)
+sales<- readRDS("./data/salesdata.rds")
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+  bydate <- eventReactive(input$showbutton,{
+    sequence <- seq(as.Date(paste(input$id2,input$id1,"01",sep = "/")), as.Date(paste(input$id4,input$id3,"01",sep = "/")), by="month")
+    sales2  <- filter(sales, date %in% sequence) %>% group_by(group, item) %>% summarise(quantity=sum(quantity), weight = sum(weight), value= sum(value))
+    sales2
   })
   
-})
+  percentcal <- reactive({sales2 <- bydate()
+  sales3 <- group_by(sales2, group) %>% summarise(weight=sum(weight), value= sum(value))
+  sales2 <- merge(sales2,sales3, by= "group", all.x = T)
+  sales2 <- data.frame(group=sales2$group, item=sales2$item, quantity=sales2$quantity, 
+                       weight = sales2$weight.x, value= sales2$value.x,
+                       gpercentweight= round((sales2$weight.x/sales2$weight.y)*100,2),
+                       gpercentvalue= round((sales2$value.x/sales2$value.y)*100,2)) 
+  sales2$tpercentweight <- round((sales2$weight/sum(sales2$weight))*100,2)
+  sales2$tpercentvalue <- round((sales2$value/sum(sales2$value))*100,2)
+  sales2})
+  
+  selectgroup <- reactive({ sales2 <- percentcal()
+  sales2 <- filter(sales2, group %in% input$group)
+  sales2
+  })
+  
+  
+  final <-reactive(DT::datatable(selectgroup(),rownames = F,options = list(lengthMenu=c(5,20,50)),
+                  colnames = c("Group","Product","Quantity(Pcs)","Weight(Kgs)",
+                               "Value(Rs)","Weight(% of Group)",
+                               "Value(% of Group)", "Weight(% of Total)",
+                               "Value(% of Total)")))
+  output$mytable <- DT::renderDataTable(final())
+}
+)
--- ProductSalesReport/ui.R
+++ ProductSalesReport/ui.R
@@ -1,33 +1,39 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+monthlist <- list("January"=1, "February"=2, "March"=3, "April"=4, "May"=5,
+                  "June"=6, "July"=7, "August"=8, "September"=9, "October"=10,
+                  "November"=11, "December"=12)
+group.name <- list("Cloth Clips","Tray & Plates","Lunch Box","Water Bottle", 
+                   "Food Store Container & Bowls", "Other Items",
+                   "Glasses", "Soap Case & Bath Tumblers", "Compass Box",
+                   "Meal Set Items & Gift Set Items", "Stationery Items")
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+  fluidRow(img(src="logo.png", height=62,width=322)),
+  br(),
+  p("HomeLine Products is a Manufacturing Firm that produces a wide variety of 
+    plastic products. These products can be broadly classified into 11 different
+    categories (Names of these categories can be found out from the Control
+    Group Visibility widget)."),
+  p("This app allows the user to see the sales of each product over a desired
+    span of time and shows how much a particular product contributes to the 
+    overall sales as well as the sales of the particular category to which the
+    product belongs. The app contains data from January 2015 to March 2016."),
+  strong("IMPORTANT NOTE- The sales numbers have been randomised and manipulated 
+    in order to avoid disclosing confidential company data."),
+  hr(),
+  fluidRow(
+    column(2,
+           selectInput("id1", label = "Begning Month", choices = monthlist),
+           selectInput("id2", label = "Begning Year", choices = list("2015"=2015,"2016"=2016)),
+           actionButton("showbutton", "Show")),
+    column(2,
+           selectInput("id3", label = "Ending Month", choices = monthlist),
+           selectInput("id4", label = "Ending Year", choices = list("2015"=2015,"2016"=2016))),
+    column(6, offset = 1,
+           selectizeInput(
+             "group", "Control Group Visibility", choices = group.name, multiple = TRUE))
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
+  hr(),
+  DT::dataTableOutput("mytable")
+  
 ))
